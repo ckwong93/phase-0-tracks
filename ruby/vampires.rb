e95a1b4200c3a9c5c8864ab22bdb07b70f439ee2
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-04 12:40:37 -0500

    initil commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+
+puts "Our company cafeteria serves garlic bread.Should we order
+some for you?(yes or no)"
+bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?(yes or no)"
+health = gets.chomp
