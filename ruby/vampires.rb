2c2ddd985fdcd01f9fcf68e578d247243cf45e52
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-06 18:14:04 -0500

    add questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+
+puts "What's your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year = gets.chomp.to_i
+
+puts "Should we order some garlic bread for you?(y/n)"
+g_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?(y/n)"
+insurance = gets.chomp
