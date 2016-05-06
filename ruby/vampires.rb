359cb1e69a5ac95a3a1dd1c99b783f447354cb12
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-06 18:25:06 -0500

    testing question

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,6 @@
+require 'date'
+current_year = Date.today.year
+current_year = current_year.to_i
 
 puts "What's your name?"
 name = gets.chomp
@@ -13,3 +16,17 @@ g_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?(y/n)"
 insurance = gets.chomp
+
+#Testing
+
+if name == "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire"
+elsif age != current_year - year && (g_bread == "n" || insurance == "n")
+  puts "Probably a vampire"
+elsif age == current_year - year && (g_bread == "y" || insurance == "y")
+  puts "Probably not a vampire"
+elsif age != current_year - year && (g_bread == "n" && insurance == "n")
+  puts "Almost certainly a vampire"
+else
+  puts "Results inconclusive"
+end
