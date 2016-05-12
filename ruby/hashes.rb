e481dada70b4dc7870b3de6f248688473f7fb9c4
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-12 14:47:46 -0500

    client's information

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+# Getting Client's Information
+
+puts "Hello,please type your name:"
+name = gets.chomp.to_s.capitalize
+
+puts "What's your age?"
+age = gets.chomp.to_i
+
+puts "How many children do you have?"
+children = gets.chomp.to_s
+
+puts "Lastly, what decor theme would you like?"
+decor = gets.chomp.to_s
