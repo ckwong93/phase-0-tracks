e9f76017d1e512c4101fe2cb68919faa3ddc61ae
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-13 16:47:28 -0500

    update changes to loop if needed

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,8 +4,8 @@ system "clear"
 client_info = {}
 
 def title
-	puts title = "Interior Design Interview Questions"
-	puts "------------------------------------"
+	puts title = "Interior Design Questions"
+	puts "-------------------------"
 end
 
 
@@ -36,36 +36,52 @@ client_info.each {|x, y| puts "#{x}:#{y}"}
 puts ""
 
 #update changes if needed
-while update != 'none'
 
-puts "Type the item that needs to be changed (name, age, children, decor). If nothing needs to be changed please type 'none' or 'no'."
-update = gets.chomp.to_s
-
-if update == "name"
-  puts "Update your name if needed:"
-  name = gets.chomp.to_s
-  client_info[:User] = name
-
-elsif update == "age"
-  puts "Update your age"
-  age = gets.chomp.to_i
-  client_info[:Age] = age
-
-elsif update == "children"
-  puts "Update, how many children you have"
-  children = get.chomp.to_i
-  client_info[:Kids] = children
-
-elsif update == "decor"
-  puts "Have you changed you mind on your decor theme? If so, tell us which one you prefer (Modern, Country, Rustic, Contemporary)."
-  decor = gets.chomp.to_s
-  client_info[:Theme] = decor
-
-else
-  puts "Thanks! Have a great day!"
-end
-
-end
-print "Client Information"
 puts ""
-client_info.each {|x, y| puts "#{x}:#{y}"}
+
+
+	puts "Type the item that needs to be changed (name, age, children, decor). If
+	nothing needs to be changed please type 'none'."
+	update = gets.chomp.to_s.downcase
+
+	while update !="none" do
+
+	if update == "name"
+	  puts "Update your name if needed:"
+	  name = gets.chomp.to_s
+	  client_info[:User] = name
+		puts ""
+
+	elsif update == "age"
+	  puts "Update your age"
+	  age = gets.chomp.to_i
+	  client_info[:Age] = age
+		puts ""
+
+	elsif update == "children"
+	  puts "Update, how many children you have"
+	  children = get.chomp.to_i
+	  client_info[:Kids] = children
+		puts ""
+
+	elsif update == "decor"
+	  puts "Have you changed you mind on your decor theme? If so, tell us which one you prefer (Modern, Country, Rustic, Contemporary)."
+	  decor = gets.chomp.to_s
+	  client_info[:Theme] = decor
+		puts ""
+
+	else
+	  puts "Thanks! Have a great day!"
+	end
+
+	puts "Type the item that needs to be changed (name, age, children, decor). If nothing needs to be changed please type 'none' or 'no'."
+	update = gets.chomp.to_s.downcase
+
+	puts ""
+	print "Client Information"
+	puts ""
+	client_info.each {|x, y| puts "#{x}:#{y}"}
+
+
+
+end
