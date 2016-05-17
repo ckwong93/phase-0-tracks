3675a1f3a690897cc8faa7e422ff29f060bcfb2e
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-16 23:16:16 -0500

    release 0 done

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
-end
+  def speak(i)
+  i.times {p "woof"}
+  end
 
+  def roll_over
+    puts "roll over"
+  end
+
+  def dog_years(human_years)
+  puts human_years / 7
+  end
+
+  def jump
+    puts "Jump on Audrey"
+
+  end
+
+
+end
+chewy = Puppy.new
+chewy.speak(5)
+chewy.fetch("ball")
+chewy.roll_over
+chewy.dog_years(14)
+chewy.jump
