e07a25df9dad8eda47d0919facddc9bb2e60da74
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-17 00:14:19 -0500

    final puppy_methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+    puts "Initalizing new puppy instance...."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -19,7 +23,6 @@ class Puppy
 
   def jump
     puts "Jump on Audrey"
-
   end
 
 
@@ -30,3 +33,42 @@ chewy.fetch("ball")
 chewy.roll_over
 chewy.dog_years(14)
 chewy.jump
+
+#Write your own class
+
+class Cooking
+
+  def initialize
+    puts "Welcome to your first cooking class!"
+  end
+
+  def knife(i)
+    i.times{p "chop"}
+  end
+
+  def seasoning(x)
+    x.times{p "shake"}
+  end
+
+  def secret_ingredient(ing)
+    puts "The only ingredient you need is #{ing}"
+  end
+end
+
+#looping
+
+cooking_array = []
+i = 1
+until i > 50
+cooking_array[i] = Cooking.new
+cooking_array << i
+i += 1
+end
+
+#call the methods
+
+ObjectSpace.each_object Cooking do |obj|
+obj.knife(5)
+obj.seasoning(5)
+obj.secret_ingredient("salt")
+end
