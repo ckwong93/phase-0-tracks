0725890bba4bf34d82a9a3d20f706866212eff29
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-23 19:48:42 -0500

    added release 0 and 1

---
+++ ruby/santa.rb
@@ -0,0 +1,20 @@
+class Santa
+  def initialize(argument)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
+    "Comet", "Cupid", "Donner", "Blitzen"]
+    print "Initializing Santa instance..."
+    @age = 0
+  end
+
+  def speak
+    print "Ho, Ho, Ho! Haaaappy Holidays!"
+
+
+  end
+
+  def eat_milk_and_cookies
+    print "That was a good #{}!"
+
+  end
