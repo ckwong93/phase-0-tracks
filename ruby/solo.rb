39593f69b206b93352e23eee084ec5082a0dfae0
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-24 19:50:02 -0500

    Final solo

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -12,78 +12,94 @@ end
 title
 
 class Pizza
-  attr_accessor :delivery, :crust, :toppings
+	attr_accessor :crust, :toppings, :client_info
 
   def initialize
-    @delivery = delivery
     @crust = crust
-    @toppings = []
+    @toppings = toppings
   end
 
-  def crust
-    puts "What type of crust would you like? (Pan, Thin, or Deep Dish)"
-    @crust = gets.chomp.to_s.capitalize
+  def pick_up_order_type
+  	@pick_up_order_type = pick_up_order_type
   end
 
-  def toppings
-		p "Type 'done' when finished picking toppings."
-		puts "Vegetables: Green peppers, Mushrooms, Onions, Olives, Broccoli
-    Meats:Bacon, Beef, Chicken, Ham, Pepperoni "
-    until (@toppings == "done") do
-    @toppings = gets.chomp.to_s.downcase
-    end
-  end
+  def order_confirmation
+  	@client_info = client_info
 
-	def pick_up_order_type
-    client_info = {}
-    puts "Delivery or pickup"
-    order = gets.chomp.to_s.downcase
-
-    if order == "delivery"
-      puts "Name: "
-      name = gets.chomp.to_s.capitalize
-      client_info[:Name] = name
-
-      puts "Address: "
-      address = gets.chomp
-      client_info[:Address] = address
-
-      puts "City: "
-      city = gets.chomp.to_s.capitalize
-      client_info[:City] = city
-
-      puts "State: "
-      state = gets.chomp.upcase
-      client_info[:State] = state
-
-      puts "Zipcode: "
-      zipcode = gets.chomp.to_i
-      client_info[:Zipcode] = zipcode
-
-      puts "Phone number: "
-      number = gets.chomp.to_i
-      client_info[:PhoneNumber] = number
-
-		  print "Client Information"
+  	print "Client Information"
 		  puts "-------------------"
 		  puts ""
-		  client_info.each {|x, y| puts "#{x}:#{y}"}
 
-    else
-      print "Your order will be ready 30 mins!"
-    end
   end
-
-
-  def print_order(client_info)
-    print "Client Information"
-    puts "-------------------"
-    puts ""
-    client_info.each {|x, y| puts "#{x}:#{y}"}
-  end
-
 end
 
-pizza_order = Pizza.new
-pizza_order.pick_up_order_type
-pizza_order.toppings
+client_info = {}
+puts "Delivery or pickup"
+order = gets.chomp.to_s.downcase
+
+if order == "delivery"
+puts "Name: "
+name = gets.chomp.to_s.capitalize
+client_info[:Name] = name
+
+puts "Address: "
+address = gets.chomp
+client_info[:Address] = address
+
+puts "City: "
+city = gets.chomp.to_s.capitalize
+client_info[:City] = city
+
+puts "State: "
+state = gets.chomp.upcase
+client_info[:State] = state
+
+puts "Zipcode: "
+zipcode = gets.chomp.to_i
+client_info[:Zipcode] = zipcode
+
+puts "Phone number: "
+number = gets.chomp.to_i
+client_info[:PhoneNumber] = number
+
+puts "What type of crust would you like? (Pan, Thin or Deep-Dish)"
+@crust = gets.chomp.to_s.downcase
+client_info[:Crust] = @crust
+
+
+puts "Type 'done' when finished picking toppings."
+puts "Vegetables: Green peppers, Mushrooms, Onion,Olives, Broccoli
+Meats:Bacon, Beef, Chicken, Ham, Pepperoni"
+until @toppings == "done" do
+@toppings = gets.chomp.to_s.downcase
+client_info[:Toppings] = @toppings
+
+end
+else
+
+puts "Name: "
+name = gets.chomp.to_s.capitalize
+client_info[:Name] = name
+
+puts "Phone number: "
+number = gets.chomp.to_i
+client_info[:PhoneNumber] = number
+
+puts "What type of crust would you like? (Pan, Thin or Deep-Dish)"
+@crust = gets.chomp.to_s.downcase
+client_info[:Crust] = @crust
+
+puts "Type 'done' when finished picking toppings."
+puts "Vegetables: Green peppers, Mushrooms, Onion,Olives, Broccoli
+Meats:Bacon, Beef, Chicken, Ham, Pepperoni"
+
+until @toppings == "done" do
+@toppings = gets.chomp.to_s.downcase
+client_info[:Toppings] = @toppings
+
+end
+end
+
+pizza = Pizza.new
+p pizza.order_confirmation
+client_info.each {|x, y| puts "#{x}:#{y}"}
