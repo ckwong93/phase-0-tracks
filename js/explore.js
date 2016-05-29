68f3f252652b0ea9392f86b871f83e896248eff0
“Anish <mailto:anish.gheewala@gmail.com>
2016-05-29 18:05:24 -0500

    final function

--- js/explore.js
+++ js/explore.js
@@ -3,13 +3,23 @@
 //Set an index = to 0 and store a emtpy string
 //Loop through the index and call each index value until you reach the end of the string
 //This will store each value into the string and return a reversed string
-function reverse(str) {
 
+
+function reverse(str) {
+// spliting the string
 	str = str.split("")
+// reversing the string
   str = str.reverse();
+// joing the string after splitting and reversing the string
 	str = str.join("")
 
   return str;
 
 }
+//driver code
 console.log(reverse("hello"))
+
+function multiply(x, y) {
+   return x * y;
+}
+console.log(multiply(3, 3))
